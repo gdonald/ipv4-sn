8c76b92e49e06e2cc9fd99649504c53e4be8dc04
Greg Donald <gdonald@gmail.com>
2021-02-13 12:08:48 -0600

    bootstrap

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+
+$(function() {
+    $('#address').change(function() {
+        console.log('address changed');
+    });
+
+    $('#mask').change(function() {
+        console.log('mask changed');
+    });
+});
+
 function validIPv4(value) {
     return false;
 }
